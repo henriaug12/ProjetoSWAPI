acf32acf8e7d4b38e7d6acabc09d83f2cc3daf19
henrique <henriaug12@gmail.com>
2023-10-12 17:06:14 -0300

    Função AllCharacters e CountPages adicionada

--- Program.cs
+++ Program.cs
@@ -2,10 +2,10 @@
 using System.Text.Json;
 using System.Text.Json.Nodes;
 public class Program {
-    static async Task getMovies(HttpClient client){
+    static async Task GetFilms(HttpClient client){
         try{
-            var json = await client.GetStringAsync(
-             "https://swapi.dev/api/films/");
+            var url = "https://swapi.dev/api/films/";
+            var json = await client.GetStringAsync(url);
             
             JsonNode? filmListNode = JsonNode.Parse(json);
             if(filmListNode is not null){
@@ -18,16 +18,16 @@ public class Program {
             }
             else Console.WriteLine("Json node was null");
         }
-        catch (Exception e)
+        catch (NullReferenceException)
         {
-            MyExceptionHandler eHandler = new(e);
+            throw new NullReferenceException();
         }
         
     }
-    static async Task getCharacters(HttpClient client){
+    static async Task GetCharacters(HttpClient client){
         try{
-            var json = await client.GetStringAsync(
-             "https://swapi.dev/api/people/");
+            var url = "https://swapi.dev/api/people/";
+            var json = await client.GetStringAsync(url);
             
             JsonNode? characterListNode = JsonNode.Parse(json);
             if(characterListNode is not null){
@@ -40,15 +40,15 @@ public class Program {
             }
             else Console.WriteLine("Json node was null");
         }
-        catch (Exception e)
+        catch (NullReferenceException)
         {
-            MyExceptionHandler eHandler = new(e);
+            throw new NullReferenceException();
         }
     }
-    static async Task getPlanets(HttpClient client){
+    static async Task GetPlanets(HttpClient client){
         try{
-            var json = await client.GetStringAsync(
-             "https://swapi.dev/api/planets/");
+            var url = "https://swapi.dev/api/planets/";
+            var json = await client.GetStringAsync(url);
             
             JsonNode? planetListNode = JsonNode.Parse(json);
             if(planetListNode is not null){
@@ -61,11 +61,72 @@ public class Program {
             }
             else Console.WriteLine("Json node was null");
         }
-        catch (Exception e)
+        catch (NullReferenceException)
         {
-            MyExceptionHandler eHandler = new(e);
+            throw new NullReferenceException();
         }
     }
+    
+    static async Task AllCharacters(HttpClient client){
+        try{
+            var url = "https://swapi.dev/api/people/";
+            var json = await client.GetStringAsync(url);
+            
+            await CountPages(client, url);
+
+            JsonNode? characterListNode = JsonNode.Parse(json);
+            if(characterListNode is not null){
+            
+                var results = JsonSerializer.Deserialize<List<Character>>(characterListNode["results"]);
+                var nextUrl = JsonSerializer.Deserialize<string>(characterListNode["next"]);
+
+                while(nextUrl is not null){
+                    json = await client.GetStringAsync(nextUrl);
+                    characterListNode = JsonNode.Parse(json);
+                    results.AddRange(JsonSerializer.Deserialize<List<Character>>(characterListNode["results"]));
+                    nextUrl = JsonSerializer.Deserialize<string>(characterListNode["next"]);
+                }
+                
+                foreach(var character in results){
+                    Console.WriteLine($"{character.Name}: {character.Gender}"+
+                        $", born in {character.Birth_year}");
+                } 
+            }
+            else Console.WriteLine("Json node was null");
+        }
+        catch (NullReferenceException)
+        {
+            throw new NullReferenceException();
+        }
+    }
+
+    static async Task CountPages(HttpClient client,string url){
+        try{
+            var json = await client.GetStringAsync(url);
+            JsonNode? pageCountNode = JsonNode.Parse(json);
+            if(pageCountNode is not null){
+                var previousUrl = JsonSerializer.Deserialize<string>(pageCountNode["previous"]);
+                while(previousUrl is not null){
+                    json = await client.GetStringAsync(previousUrl);
+                    pageCountNode = JsonNode.Parse(json);
+                    previousUrl = JsonSerializer.Deserialize<string>(pageCountNode["previous"]);
+                }
+                int count = 1;
+                var nextUrl = JsonSerializer.Deserialize<string>(pageCountNode["next"]);
+                while(nextUrl is not null){
+                    count += 1;
+                    json = await client.GetStringAsync(nextUrl);
+                    pageCountNode = JsonNode.Parse(json);
+                    nextUrl = JsonSerializer.Deserialize<string>(pageCountNode["next"]);
+                }
+                Console.WriteLine($"{count} paginas");
+            }
+        }
+        catch(NullReferenceException){
+            throw new NullReferenceException();
+        }
+    }
+
     static async Task Main(string[] args){
         HttpClient client = new();
         client.DefaultRequestHeaders.Accept.Clear();
@@ -80,26 +141,24 @@ public class Program {
             var input = Console.ReadLine();
             if (int.TryParse(input, out option))
             {
-                switch (option)
-                {
-                    case 1: Console.WriteLine("Please wait...\n");
-                    await getMovies(client);
-                    break;
+                if(option != -1){
+                    Console.WriteLine("Please wait...\n");
+                    switch (option)
+                    {
+                        case 1: await GetFilms(client);
+                        break;
 
-                    case 2: Console.WriteLine("Please wait...\n");
-                    await getCharacters(client);
-                    break;
+                        case 2: await GetCharacters(client);
+                        break;
 
-                    case 3: Console.WriteLine("Please wait...\n");
-                    await getPlanets(client);
-                    break;
-
-                    case -1: 
-                    break;
-                    
-                    default: Console.WriteLine("Number not within acceptable range");
-                    break;
+                        case 3: await GetPlanets(client);
+                        break;
+                        
+                        default: Console.WriteLine("Number not within acceptable range");
+                        break;
+                    }
                 }
+                else Console.WriteLine("Exiting...");
             }
             else Console.WriteLine("Input was not an integer number");
         }
